d7eb1b4dce3e2d0474fbbeddbf1678360c593625
2523570 <2523570@gs.seito-fku.ed.jp>
2026-02-04 13:53:12 +0900

    Update combatResolution.js

--- logic/combatResolution.js
+++ logic/combatResolution.js
@@ -1,8 +1,8 @@
 // combatResolution.js
 // Resolves spell effects, dice rolls, and damage vs Yokai
 
-import { rollDice } from "./logic/dice.js";
-import { getElementBonusDice } from "./logic/elementBonus.js";
+import { rollDice } from "./dice.js";
+import { getElementBonusDice } from "./elementBonus.js";
 
 /**
  * Resolves combat for a single round
